5ca39e9b1894016c615532c644fc0570ea89d197
UnderTheKnife <bogdanbelous@ukr.net>
2017-07-19 10:06:11 +0300

    lesson_14

--- lessons/lesson_14/js/index.js
+++ lessons/lesson_14/js/index.js
@@ -75,3 +75,27 @@ function task_5() {
     }
     display(5, answer)
 }
+
+function task_6() {
+    var arr = [[1, 2, 3], [4, 5], [6]],
+        sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            sum += arr[i][j];
+        }
+    }
+    display(6, sum)
+}
+
+function task_7() {
+    var arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]],
+        sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            for (var n = 0; n < arr[i][j].length; n++) {
+                sum += arr[i][j][n];
+            }
+        }
+    }
+    display(7, sum)
+}
