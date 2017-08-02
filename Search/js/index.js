bc95f359d7308125e58693388b4a3f0b0ce4cb2e
UnderTheKnife <bogdanbelous@ukr.net>
2017-08-02 10:34:17 +0300

    init

---
+++ Search/js/index.js
@@ -0,0 +1,7 @@
+function search() {
+  const string = window.document.getElementById('string');
+}
+
+window.addEventListener('load', () => {
+  window.document.getElementById('search').addEventListener('click', search);
+});
